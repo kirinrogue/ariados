4811a3a53689f804edd4b1b01995ecd32a369c1e
KirinAnks <fjgparrales@gmail.com>
2018-05-14 16:11:22 +0200

    add: rest_auth y urls

--- ariados_project/apps/authentication/urls.py
+++ ariados_project/apps/authentication/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.show_users),
+    url(r'^$', include('rest_auth.urls')),
     url(r'^login/', views.handle_login),
     url(r'^logout/', views.handle_logout),
     url(r'^users/', views.show_users),
