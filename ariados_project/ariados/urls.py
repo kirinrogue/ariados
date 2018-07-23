50dd4a4746fff08f8540f860d48bac493e5a33d0
KirinAnks <fjgparrales@gmail.com>
2018-07-23 18:02:35 +0200

    add: urls de posts

--- ariados_project/ariados/urls.py
+++ ariados_project/ariados/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^auth/', include('apps.authentication.urls')),
     url(r'^trainers/', include('apps.trainers.urls')),
+    url(r'^posts/', include('apps.posts.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
